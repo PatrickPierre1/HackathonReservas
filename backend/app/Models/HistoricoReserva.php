c0197dd03602ed7aa8412fcaeda89183aa66874e
joaopedro <joaopkrodrigues@gmail.com>
2024-11-22 19:10:12 -0300

    Adicionando Histórico de Reservas

--- backend/app/Models/HistoricoReserva.php
+++ backend/app/Models/HistoricoReserva.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class HistoricoReserva extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'historico_reservas';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'reserva_id',
+        'alteracoes',
+    ];
+
+    /**
+     * Get the reserva associated with the historico.
+     */
+    public function reserva()
+    {
+        return $this->belongsTo(Reserva::class);
+    }
 }
